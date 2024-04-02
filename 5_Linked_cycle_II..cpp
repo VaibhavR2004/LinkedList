5a24f46ea3b6c75b3018bad3c7e94914be156a80
VaibhavR2004 <150423879+VaibhavR2004@users.noreply.github.com>
2024-04-02 23:31:37 +0530

    Create 5_Linked_cycle_II..cpp

---
+++ 5_Linked_cycle_II..cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ https://leetcode.com/problems/linked-list-cycle-ii/
+ */
+class Solution {
+public:
+    ListNode *floydDetectLoop(ListNode *head) {
+        if (head ==  NULL){
+            return NULL;
+        }
+        ListNode* slow =  head;
+        ListNode* fast = head;
+
+        while (slow!=NULL && fast!=NULL){
+            fast = fast->next;
+            if (fast != NULL){
+                fast= fast->next;
+            }
+            slow = slow->next;
+            if (slow == fast){
+                return slow;
+            }
+        }
+     return NULL;   
+    }
+    ListNode* detectCycle(ListNode *head){
+        if(head == NULL){
+            return NULL;
+        }
+        ListNode* intersection = floydDetectLoop(head);
+        if(intersection == NULL){
+            return NULL;
+        }
+        ListNode* slow = head;
+        while (slow!=intersection){
+            slow= slow->next;
+            intersection = intersection->next;
+        }
+        return slow;
+    }
+};
