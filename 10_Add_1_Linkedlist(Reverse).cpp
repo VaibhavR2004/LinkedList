f629769ff9869ba2c9e623ecfd4fd8ac56b70a79
VaibhavR2004 <150423879+VaibhavR2004@users.noreply.github.com>
2024-04-03 23:27:17 +0530

    Create 10_Add_1_Linkedlist(Reverse).cpp

---
+++ 10_Add_1_Linkedlist(Reverse).cpp
@@ -0,0 +1,56 @@
+class Solution
+{
+    public:
+    Node* reverse(Node* head){
+        if(head==NULL||head->next==NULL){
+    return head;
+}
+        Node* prev = NULL;
+        Node* curr = head;
+
+        while(curr != NULL){
+            Node* forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+            
+        }
+        return prev;
+    }
+    Node* addOne(Node *head) 
+    {
+        // Your Code here
+        // return head of list after adding one
+        if (head==NULL){
+            return NULL;
+        }
+        
+        head= reverse(head);
+        Node* temp= head;
+        int carry =1;
+        
+        while(temp!=NULL){
+            temp->data = temp->data +carry;
+            if (temp->data<10){
+                carry=0;
+                break;
+            }
+            else{
+                temp->data=0;
+                carry=1;
+            }
+            temp= temp->next;
+            
+        }
+        if(carry ==1){
+            Node* newNode= new Node(1);
+            head =reverse(head);
+            newNode -> next = head;
+            return newNod;
+        }
+        head = reverse(head);
+        return head;
+        
+        
+    }
+};
